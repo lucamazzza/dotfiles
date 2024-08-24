e719979aabf93bb810fdaff82776ecab03c9b8d3
Luca Mazza <luca.mazza@mazluc.ch>
2024-08-24 14:22:28 +0200

    Workspace for dotfiles

--- .config/nvim/after/plugin/workspace.lua
+++ .config/nvim/after/plugin/workspace.lua
@@ -3,6 +3,7 @@
     workspaces = {
         {name = "Work", path = "~/Projects", keymap = {"<leader>w"}},
         {name = "School", path = "~/School", keymap = {"<leader>s"}},
+        {name = "Dotfiles", path = "~/dotfiles/.config", keymap = {"<leader>z"}},
     }
  })
  vim.keymap.set('n', '<leader>ts', workspace.tmux_sessions)
