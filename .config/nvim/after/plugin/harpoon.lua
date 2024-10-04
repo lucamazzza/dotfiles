099e4297762aea6cb68fe657cecc78e119c2a0e7
Luca Mazza <luca.mazza@mazluc.ch>
2024-10-04 09:11:02 +0200

    harpoon size

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -1,3 +1,10 @@
+require("harpoon").setup({
+    menu = {
+        width = vim.api.nvim_win_get_width(0) - 80,
+        height = vim.api.nvim_win_get_height(0) - 40,
+    }
+})
+
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
